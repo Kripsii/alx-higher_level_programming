ad8eb286603cc231d497731fe851f0677c726dfe
Kripsii <alekimaina64@gmail.com>
2022-06-02 14:25:53 -0700

    README.md

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import hidden_4 as hidden
-    for name in dir(hidden):
-        if not name.startswith("__"):
-             print(name)
+import hidden_4
+
+if __name__ == '__main__':
+    def vdir(hidden_4):
+        if hidden_4 == None:
+            return None
+        return [x for x in dir(hidden_4) if not x.startswith('__')]
+    print('\n'.join(str(x) for x in vdir(hidden_4)))
