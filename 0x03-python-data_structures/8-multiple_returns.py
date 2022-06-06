e64b355c7845862752ddbccb440a160c85fbd441
Kripsii <alekimaina64@gmail.com>
2022-06-06 13:41:13 -0700

    README.md

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if not sentence:
-        my_tuple = 0, "None"
-    else:
-        my_tuple = len(sentence), sentence[0]
-        return my_tuple
+    if sentence == 0:
+        return (0, None)
+    return (len(sentence), sentence[0])
