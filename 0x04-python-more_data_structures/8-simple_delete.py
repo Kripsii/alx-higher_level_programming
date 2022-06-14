f77e7fd196e8ad7ab6fc39f0e5f030a18d011e3d
Kripsii <alekimaina64@gmail.com>
2022-06-14 16:09:14 -0700

    README.md

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if key not in a_dictionary:
-        pass
-    else:
-        a_dictionary.pop(key)
+    """
+    deletes an element based on the key from a dictionary
+    """
+    if key in a_dictionary:
+        del a_dictionary[key]
         return a_dictionary
