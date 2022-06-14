43ee200f80814bb28e2f45c8d74175ceb7dc80cd
Kripsii <alekimaina64@gmail.com>
2022-06-14 15:23:36 -0700

    README.md

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-
-nc = 0
-for n in range(122, 96, -1):
-    if n % 2 == 0:
-        nc = n
-    else:
-        nc = n - 32
-        print("{}".format(chr(nc)), end='')
+for c in range(ord('z'), ord('a') - 1, -2):
+    print("{:c}{:s}".format(c, chr(c - 33)), end="")
