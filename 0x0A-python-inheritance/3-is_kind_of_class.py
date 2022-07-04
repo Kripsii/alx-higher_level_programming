99643c5e31d6ec69c9ecd66aad58a58e98a2557e
Kripsii <alekimaina64@gmail.com>
2022-07-04 14:45:35 -0700

    Same class or inherit from

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" This module defines a function is_kind_of_class. """
+
+
+def is_kind_of_class(obj, a_class):
+    """ Return True if isinstance else False. """
+    return isinstance(obj, a_class)
