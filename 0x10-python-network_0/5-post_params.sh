a69ca41f1b24ba11f9804deaf375635207d3f3ec
Kripsii <alekimaina64@gmail.com>
2022-09-26 06:28:42 -0700

    Update solutions

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
-#!/bin/bah
-# sends a POST request to the passed URL, and displays the body of the response
-curl - sd "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+#!/bin/bash
+# Use a POST request
+curl -s "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
