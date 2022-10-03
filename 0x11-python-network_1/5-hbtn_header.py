4897bcf2ea94e33f835ced6d278748cff417d4b2
Kripsii <alekimaina64@gmail.com>
2022-10-03 08:58:53 -0700

    Update

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""takes in a URL, sends a request to the URL
-and displays the value of the
-variable X-Request-Id"""
-from requests import get
+
+""" Uses requests module yo get header info. """
+
+import requests
 from sys import argv
 
-
 if __name__ == '__main__':
-    url = argv[1]
-    response = get(url)
-    info = response.
-    print(info.get('X-Request-Id'))
+    response = requests.get(argv[1])
+    print(response.headers.get('X-Request-Id'))
