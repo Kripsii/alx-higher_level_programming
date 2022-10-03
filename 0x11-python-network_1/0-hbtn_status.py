5ea58240697a08c4d483a32fd5d9792806999f0a
Kripsii <alekimaina64@gmail.com>
2022-10-03 07:17:11 -0700

    Up-to date commit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
             bytes_content = response.read()
             content = bytes_content.decode('utf-8')
             string = 'Body response:\n\t- type: {}\n\t- content: {}\n\t- \
-    utf8 content: {}'.format(type(bytes_content), bytes_content, content)
-        print(string)
+                    utf8 content: {}'.format(type(bytes_content), bytes_content, content)
+                    print(string)
