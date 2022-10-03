b3ee5050f3500e0d1d5826bcbbecadbba64ac472
Kripsii <alekimaina64@gmail.com>
2022-10-03 07:53:17 -0700

    POST an email #1

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""takes in a URL and an email address, sends a POST request
+to the passed URL with the email as a parameter"""
+from sys import argv
+from requests import post
+
+
+if __name__ == '__main__':
+    url = argv[1]
+    email = {'email': argv[2]}
+    response = post(url, data=email)
+    print(response.text)
