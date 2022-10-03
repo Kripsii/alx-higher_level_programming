115ef4c4542b6374596e5f9be19e6b4793f10352
Kripsii <alekimaina64@gmail.com>
2022-10-03 07:04:56 -0700

    My status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Fetches a url """
+
+from urllib.request import urlopen
+
+if __name__ == "__main__":
+    if __name__ == "__main__":
+        with urlopen(url) as response:
+            bytes_content = response.read()
+            content = bytes_content.decode('utf-8')
+            string = 'Body response:\n\t- type: {}\n\t- content: {}\n\t- \
+    utf8 content: {}'.format(type(bytes_content), bytes_content, content)
+        print(string)
